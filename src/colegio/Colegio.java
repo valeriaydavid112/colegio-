86aec85235904996d2f8875188565aded5a7857f
valeria y david <valeria.camacho@mariaelvinia.edu.co>
2019-10-08 12:52:40 -0500

    Initial commit

---
+++ src/colegio/Colegio.java
@@ -0,0 +1,10 @@
+
+package colegio;
+
+public class Colegio {
+    private String colorDePupitres;
+    private int numeroDeProfesores; 
+    private int numeroDeComputadores;
+    private boolean certificadoDeSeguridad;
+    private Date fechaDeFundacon;
+}
